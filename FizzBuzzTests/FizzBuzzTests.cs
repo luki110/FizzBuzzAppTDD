e502e2ac17566365b024e6475373c1e207e31ab7
luki110 <lukas.bonkowski@gmail.com>
2024-07-17 20:11:28 +0100

    added test cases and FizzBuzz class

--- FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -1,16 +1,57 @@
+using FizzBuzzAppTDD;
+
 namespace FizzBuzzTests
 {
-    public class Tests
+    [TestFixture]
+    public class FizzBuzzTest
     {
+        private FizzBuzz _fizzBuzz;
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            _fizzBuzz = new FizzBuzz();
+
         }
 
         [Test]
-        public void Test1()
+        public void NumberDivisibleByThree_ShouldReturnFizz()
         {
-            Assert.Pass();
+            var actual = "Fizz";
+
+            var result = _fizzBuzz.GetOutput(3);
+
+            Assert.That(actual, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void NumberDivisibleByFive_ShouldReturnBuzz()
+        {
+            var actual = "Buzz";
+
+            var result = _fizzBuzz.GetOutput(5);
+
+            Assert.That(actual, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void NumberDivisibleByThreeAndFive_ShouldReturnFizzBuzz()
+        {
+            var actual = "FizzBuzz";
+
+            var result = _fizzBuzz.GetOutput(15);
+
+            Assert.That(actual, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void NumberNotDivisibleByThreeOrFive_ShouldReturnNumber()
+        {
+            var actual = "1";
+            var result = _fizzBuzz.GetOutput(1);
+
+
+            Assert.That(actual, Is.EqualTo(result));
         }
     }
 }
