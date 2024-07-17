3b0e2ce0acd9277ab2f3517d13dee5f15206ce8a
luki110 <lukas.bonkowski@gmail.com>
2024-07-17 20:13:22 +0100

    added initial implementation of fizzbuzz to pass tests

--- FizzBuzzAppTDD/FizzBuzz.cs
+++ FizzBuzzAppTDD/FizzBuzz.cs
@@ -8,6 +8,16 @@ namespace FizzBuzzAppTDD
 {
     public class FizzBuzz
     {
+        public string GetOutput(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            if (number % 3 == 0)
+                return "Fizz";
+            if (number % 5 == 0)
+                return "Buzz";
 
+            return number.ToString();
+        }
     }
 }
