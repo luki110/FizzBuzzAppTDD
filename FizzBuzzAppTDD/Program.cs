a69fd464e196bb89218c54f51dbd4f56f05f852d
luki110 <lukas.bonkowski@gmail.com>
2024-07-17 20:51:09 +0100

    final implementation with use of reflection

--- FizzBuzzAppTDD/Program.cs
+++ FizzBuzzAppTDD/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzzAppTDD;
+using System.Reflection;
+
+Type fizzBuzzType = typeof(FizzBuzz);
+
+// Create an instance of the FizzBuzz class
+object fizzBuzzInstance = Activator.CreateInstance(fizzBuzzType);
+
+// Get the GetOutput method
+MethodInfo getOutputMethod = fizzBuzzType.GetMethod("GetOutput");
+
+if (getOutputMethod != null)
+{
+    // Invoke the GetOutput method for a range of numbers and print the results
+    for (int i = 1; i <= 100; i++)
+    {
+        string result = (string)getOutputMethod.Invoke(fizzBuzzInstance, new object[] { i });
+        Console.WriteLine(result);
+    }
+}
